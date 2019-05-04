51de7fcd3cf37ed7a74d4e46d4311b024306a103
David <dfernr09@estudiantes.unileon.es>
2019-05-04 18:00:44 +0200

    Añadido operaciones BBDD VehiculosVendidos

---
+++ src/Controlador/VehiculosVendidosBBDD.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import Modelo.*;
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+public class VehiculosVendidosBBDD {
+    public void nuevoVehiculoVendido(VehiculosVendidos vehiculo){
+        SessionFactory sesion = NewHibernateUtil.getSessionFactory();
+        Session session;
+        session = sesion.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(vehiculo);
+        tx.commit();
+        session.close();
+        sesion.close();
+     
+    }
+    public VehiculosVendidos buscarVehiculoVendido(int bastidorNum){
+        VehiculosVendidos vehiculo;
+        SessionFactory sesion = NewHibernateUtil.getSessionFactory();
+        Session session;
+        session = sesion.openSession();
+        Transaction tx = session.beginTransaction();
+        vehiculo = (VehiculosVendidos) session.get(VehiculosVendidos.class, bastidorNum);
+        tx.commit();
+        session.close();
+        sesion.close();
+        return vehiculo;
+    }
+    
+    public List<VehiculosVendidos> obtenerTodosVehiculosVendidos(){
+        SessionFactory sesion = NewHibernateUtil.getSessionFactory();
+        Session session;
+        session = sesion.openSession();
+        Transaction tx = session.beginTransaction();
+        Query q = session.createQuery("from VehiculosVendidos");
+        List<VehiculosVendidos> lista = q.list();
+        session.close();
+        sesion.close();
+
+        return lista;
+    }
+    
+      public void eliminarVehiculoVendido(int bastidorNum){
+        SessionFactory sesion = NewHibernateUtil.getSessionFactory();
+        Session session;
+        session = sesion.openSession();
+        Transaction tx = session.beginTransaction();
+        VehiculosVendidos vehiculo;
+        vehiculo = (VehiculosVendidos) session.get(VehiculosVendidos.class, bastidorNum);
+        if(vehiculo != null){
+            session.delete(vehiculo);
+        }
+        
+        tx.commit();
+        session.close();
+        sesion.close();
+     
+    }
+      
+     public void ponerEnTaller(int bastidorNum, boolean enTaller){
+         SessionFactory sesion = NewHibernateUtil.getSessionFactory();
+        Session session;
+        session = sesion.openSession();
+        Transaction tx = session.beginTransaction();
+        VehiculosVendidos vehiculo;
+        vehiculo = (VehiculosVendidos) session.get(VehiculosVendidos.class, bastidorNum);
+        if(vehiculo != null){
+            vehiculo.setTaller(enTaller);
+            session.update(vehiculo);
+        }
+        tx.commit();
+        session.close();
+        sesion.close();
+     }
+     
+     
+     
+}
